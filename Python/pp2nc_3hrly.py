aa50ae1f95bd4b567823b26af092fe65da06b59c
James O'Neill <earjjo@leeds.ac.uk>
2018-03-02 13:10:58 +0000

    Parameterise code better

--- Python/pp2nc_3hrly.py
+++ Python/pp2nc_3hrly.py
@@ -19,11 +19,6 @@ ncRoot=args.ncRoot
 ncRef=args.ncRef
 startDate=args.startDate
 endDate=args.endDate
-#ppRoot = '/home/users/myoshioka/ET/Retrieved/group_workspaces/jasmin2/ukca/vol1/myoshioka/um/Dumps'
-#ncRoot = '/group_workspaces/jasmin2/gassp/earjjo/NC_3hrly'
-#ncRef = '/group_workspaces/jasmin2/ukca/vol1/myoshioka/um/Dumps/Links2NC/teafw/aod550_total_teafw_pm2008jan_N48_dim_att_j.nc'
-#startDate = '20080101'
-#endDate = '20080101'
 #####
 
 #####CHECK FILES/DIRS exist
@@ -35,6 +30,12 @@ assert os.path.exists(ncRef),"ncRef does not exist"
 #####PARAMETERS
 stash_CDNC = 'm01s38i479' #STASH code for CDNC field in pp files
 stash_AOD = ['m01s02i500','m01s02i501','m01s02i502','m01s02i503','m01s02i504','m01s02i505'] #STASH codes for 6 'modes' in pp files. AOD_550 is sum over all these modes
+maxDays = 31 #Maximum number of days that can be processed in one go
+N96res=(145,192) #Expected number of lats/lons in finer resolution data
+N48res=(73,96) #Expected number of lats/lons in coarser resolution data
+nTimes=8 #Expected number of times in pp files (one day at 3-hrly resolution per file)
+nLevels=52 #Expected number vertical levels in pp files
+missing=-999 #Missing value written to nc file if pp file is skipped
 ######
 
 #####GENERATE JOBID ARRAY
@@ -79,7 +80,7 @@ except:
     raise
 numDays=(endDateObj-startDateObj).days + 1 #Add one to include both extremeties
 assert numDays > 0, "Error: startDate must come before endDate"
-assert numDays < 32, "Error: Can only process up to one month of data at a time"
+assert numDays <= maxDays, "Error: Can only process up to 31 days of data at a time"
 fileDates=[]
 for date in [startDateObj + dt.timedelta(n) for n in range(numDays)]:
     #The date.strftime() functions below ensure e.g. that the day is written as '01' and not '1'
@@ -93,7 +94,7 @@ try:
 except:
     print("Error: Couldn't load or extract coordinates from reference data file")
     raise
-assert len(lats) == 73 and len(lons) == 96, "Unexpected dimensions in reference data file"
+assert len(lats) == N48res[0] and len(lons) == N48res[1], "Unexpected dimensions in reference data file"
 #####
 
 #####FOR EACH DAY, LOOP OVER ALL JOBS, STORE DATA IN ONE BIG ARRAY, AND OUTPUT TO NC FILE
@@ -109,9 +110,8 @@ for day in fileDates:
     flog.close
     outFile=ncRoot+'/pb'+day+'.nc' #path to output nc file
     #Initialise big arrays (all jobs for current day):
-    missing=-999
-    CDNC_all=np.full(shape=(8,73,96,235),fill_value=missing)
-    AOD_all=np.full(shape=(8,73,96,235),fill_value=missing)
+    CDNC_all=np.full(shape=(nTimes,N48res[0],N48res[1],len(jobids)),fill_value=missing)
+    AOD_all=np.full(shape=(nTimes,N48res[0],N48res[1],len(jobids)),fill_value=missing)
     #Loop over jobs:
     for j,jobid in enumerate(jobids):
         #Open log file here, close it at end of loop to allow reading of it during execution
@@ -132,7 +132,7 @@ for day in fileDates:
             flog.write('Warning: Could not load CDNC data from input file '+ppFile+'. File has been skipped\n')
             flog.close
             continue
-        if CDNCcube.shape != (8,52,145,192):
+        if CDNCcube.shape != (nTimes,nLevels,N96res[0],N96res[1]):
             flog.write('Warning: Unexpected dimensions in input file '+ppFile+'. File has been skipped\n')
             flog.close
             continue
@@ -157,12 +157,12 @@ for day in fileDates:
             flog.close
             continue
         #***FOR NOW*** just take arithmetic mean of CDNC over all heights
-        for h in range(52):
+        for h in range(nLevels):
             if h==0:
                 CDNCsum=CDNCcube[:,h,:,:]
             else:
                 CDNCsum+=CDNCcube[:,h,:,:]
-        CDNCave=CDNCsum/52.0
+        CDNCave=CDNCsum/float(nLevels)
         #Regrid onto coarser resolution (N48):
         CNDC_N48 = CDNCave.regrid(refCube, iris.analysis.Linear())
         AOD_N48 = AODcube.regrid(refCube, iris.analysis.Linear())
@@ -173,6 +173,8 @@ for day in fileDates:
 
     #Create nc file:
     assert not (np.max(CDNC_all) == missing and np.min(CDNC_all) == missing), "No processed data"
+    flog=open(ncRoot+'/logfile.log','a')
+    flog.write("Writing to nc file: "+outFile+'\n')
     ncfile = netCDF4.Dataset(outFile,mode='w',format='NETCDF4_CLASSIC')
     #Add time dimension/variable:
     t_dim = ncfile.createDimension('time',len(times))
@@ -206,3 +208,4 @@ for day in fileDates:
     cdnc_out.units = "number per metre cubed"
     #close netCDF file
     ncfile.close()
+    flog.close
