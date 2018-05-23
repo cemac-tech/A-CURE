dbcb9522466d6665289590aba497d3306a697c9a
James O'Neill <earjjo@leeds.ac.uk>
2018-05-23 12:44:08 +0100

    Write print statements out to a log file

--- Python/collocate_UM_AERONET.py
+++ Python/collocate_UM_AERONET.py
@@ -16,12 +16,10 @@ assert os.path.exists(os.path.join(os.getenv("CIS_PLUGIN_HOME"),"cis_plugin_AERO
 
 #####PARAMETERS
 YYYYMM='200807' #Month in format YYYYMM
-ppRoot='/group_workspaces/jasmin2/ukca/vol1/myoshioka/um/Dumps' #pp root directory
 AERONETRoot='/group_workspaces/jasmin2/crescendo/Data/AERONET/AOT/ver3/LEV20/Monthly' #AERONET root directory
-UMRoot='/home/users/earjjo/GASSP_WS/aod550_total_jobid_pbYYYYMMDD_nc' #Output directory for pp->nc files
-colRoot='/home/users/earjjo/GASSP_WS/aod550_total_jobid_pbYYYYMM_station_nc' #Output directory for collocated nc files
-mavRoot='/home/users/earjjo/GASSP_WS/aod550_total_jobid_pbYYYYMM_station_mav_nc' #Output directory for monthly averaged nc files
-outRoot='/home/users/earjjo/GASSP_WS/aod550_total_jobid_pbYYYYMM_mav_nc' #Output directory for nc files over all stations
+ppRoot='/group_workspaces/jasmin2/ukca/vol1/myoshioka/um/Dumps' #pp root directory
+UMRoot='/home/users/earjjo/GASSP_WS/collocate_UM_AERONET/UMNC' #Output directory for pp->nc files
+outDir='/home/users/earjjo/GASSP_WS/collocate_UM_AERONET/OUTNC' #Output directory
 stash_AOD = ['m01s02i500','m01s02i501','m01s02i502','m01s02i503','m01s02i504','m01s02i505'] #STASH codes for 6 'modes' in pp files. AOD_550 is sum over all these modes
 missing=np.nan #Missing value written to nc file if field is missing
 run_pp2nc=True #Do we need to generate the nc files from the raw UM pp files?
@@ -59,7 +57,7 @@ jobids.append('teafw')
 for c1 in string.ascii_lowercase[7:9]:
     for c2 in string.ascii_lowercase:
       jobids.append('teb'+c1+c2)
-jobids=jobids[0:1] ####TEMPORARY CODE
+jobids=jobids[0:2] ####TEMPORARY CODE
 #####
 
 #####LOOP THROUGH AERONET FILES AND GET STATION NAMES           
@@ -74,9 +72,12 @@ for AERONETFile in AERONETFiles:
     stations.append(station)
 #####
 
+flog=open(os.path.join(outDir,'logfile.log'),'w')
+flog.close
 for j,jobid in enumerate(jobids):
-    print('=====PROCESSING JOB '+jobid+'=====')
-    
+    flog=open(outDir+'/logfile.log','a')
+    flog.write('=====PROCESSING JOB '+jobid+'=====\n')
+    flog.close
 #####GENERATE NC FILES FROM PP FILES
     if(run_pp2nc):
         for date in fileDates:
@@ -87,10 +88,14 @@ for j,jobid in enumerate(jobids):
             ppPath=ppRoot+'/'+jobid+'/'+ppFile #specific pp file path
             #Check input file exists, skip if not
             if not os.path.exists(ppPath):
-                print('Warning: Input file '+ppFile+' was not found. File has been skipped')
+                flog=open(outDir+'/logfile.log','a')
+                flog.write('Warning: Input file '+ppFile+' was not found. File has been skipped\n')
+                flog.close
                 continue
             #Read in all 6 cubes relating to AOD; sum to get total AOD
-            print("Extracting AOD_550 from pp file: "+ppFile)
+            flog=open(outDir+'/logfile.log','a')
+            flog.write("Extracting AOD_550 from pp file: "+ppFile+'\n')
+            flog.close
             AODcube=0
             for s,stashid in enumerate(stash_AOD):
                 modeCube=iris.load_cube(ppPath,iris.AttributeConstraint(STASH=stashid))
@@ -102,7 +107,9 @@ for j,jobid in enumerate(jobids):
                 else:
                     AODcube+=modeCube
             ####Create AOD nc file:
-            print("Writing AOD_550 to nc file: "+UMFile)
+            flog=open(outDir+'/logfile.log','a')
+            flog.write("Writing AOD_550 to nc file: "+UMFile+'\n')
+            flog.close
             ncfile = netCDF4.Dataset(UMPath,mode='w',format='NETCDF4_CLASSIC')
             #Add time dimension/variable:
             tAOD = ncfile.createDimension('time',len(AODtimes))
@@ -144,7 +151,9 @@ for j,jobid in enumerate(jobids):
     monAveDF['num'] = 0
     monAveDF['num'] = monAveDF['num'].astype(int)
     for i,AERONETFile in enumerate(AERONETFiles):
-        print("Collocating UM onto AERONET data and taking monthly average for station: "+stations[i])
+        flog=open(outDir+'/logfile.log','a')
+        flog.write("Collocating UM onto AERONET data and taking monthly average for station: "+stations[i]+'\n')
+        flog.close
         #Read in AERONET data with new plugin:
         AERONETData=cis.read_data(os.path.join(AERONETRoot,AERONETFile),"AOD_440",product="AERONETv3nc")
         #Subset AERONET data in time to ensure all points are within UM data time bounds:
@@ -155,10 +164,10 @@ for j,jobid in enumerate(jobids):
         UMData=cis.read_data(UMFiles,"aod550")
         #Collocate UM data onto AERONET data:
         colData=UMData.collocated_onto(AERONETData,how="lin",var_name="collocated_AOD550",var_units="1")
-        #colData.save_data(os.path.join(colRoot,'aod550_total_'+jobid+'_pb'+YYYYMM+'_'+station+'.nc'))
+        #colData.save_data(os.path.join(outDir,'aod550_total_'+jobid+'_pb'+YYYYMM+'_'+station+'.nc'))
         #Calculate monthly average of collocated aod550:
         aggData=colData.aggregate(how='moments',t=[monStart,monEnd,dt.timedelta(days=numDaysInMon)])
-        #aggData.save_data(os.path.join(mavRoot,'aod550_total_'+jobid+'_pb'+YYYYMM+'_'+station+'_mav.nc'))
+        #aggData.save_data(os.path.join(outDir,'aod550_total_'+jobid+'_pb'+YYYYMM+'_'+station+'_mav.nc'))
         #Add monthly average to the pandas DF:
         monAveDF.at[i,'stn']=station
         monAveDF.at[i,'lat']=aggData[0].get_all_points()[0].latitude
@@ -169,9 +178,11 @@ for j,jobid in enumerate(jobids):
 
 #####SAVE PANDAS DF TO UNGRIDDED NC FILE
     outFile='aod550_total_'+jobid+'_pb'+YYYYMM+'_mav.nc'
-    outPath = os.path.join(outRoot,outFile)
+    outPath = os.path.join(outDir,outFile)
     ncfile = netCDF4.Dataset(outPath,mode='w',format='NETCDF4_CLASSIC')
-    print("Writing data for all stations to nc file: "+outFile)
+    flog=open(outDir+'/logfile.log','a')
+    flog.write("Writing data for all stations to nc file: "+outFile+'\n')
+    flog.close
     #Add station dimension/variable. Note that multi-character strings must be handled using stringtochar:
     stn_dim = ncfile.createDimension('station',len(stations))
     nchars=len(max(stations,key=len))
